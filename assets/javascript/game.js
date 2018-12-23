f38a04a1f66af66cad0551f2a1e2c6fc2418ee20
Lucas Merat <lucas.merat@gmail.com>
2018-12-23 15:03:41 -0600

    Added styling and images to setup for JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
-$('#test').css('color','orange');
+//Pseudocode:
+//Super Smash Bros RPG!
+//Game starts, page is loaded including 4 characters to choose from 
+//Each character has health, and an attack power all within a character card div
+//Each div listens for clicks. When clicked, the person clicked should move to the "Your Character" section
+//The remaining 3 divs are moved to the "Enemies Available To Attack" section and their background goes to red
+//When one of the characters in the Available to attack section is clicked, They move to the "Defender" section and background goes to black
+//When attack is clicked, your attack triggers at a given number. Every attack, that number is doubled
+//At the same time, the enemy attacks you for a set ammt that does not increase
+//If your character health < 0, you lose, it says you are defeated, clicking attack does not work and a restart bottun pops
+//Clicking restart button reloads the page
+//if the defender's health drops below 0, it tells me I have defeated Luke and I can choose another enemy
+//If there is no enemy in the defender box, and you click attack, game tells you to choose another enemy
+//When the next character is picked, they move to defender and same cycle occurs, but the attack is still incrementing by 8
+//If you kill every team member, the game alerts you that you have won and makes it game over
+
+//Remember! You can give an image an attribute that it carries - can be a number or value later accessed
